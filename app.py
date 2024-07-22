ce7228cf28289cf623e165256d839e31293d103b
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-07-22 14:19:10 -0400

    Conección con la base de datos, encriptado de password, respuesta json

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 # requests es para extraer los datos de la petición que está haciendo el cliente
-from psycopg2 import connect
+# jsonify permite devolver un diccionario como un json
+from psycopg2 import connect, extras
+# psycopg2 es para conectarse a la base de datos
+# connect es para conectarse a la base de datos
+# extras es para obtener los datos de la base de datos no como tuplas sino como diccionarios
+from cryptography.fernet import Fernet
+# Fernet es para encriptar y desencriptar datos
+
 
 app = Flask(__name__)
+key = Fernet.generate_key()
+# Fernet.generate_key() genera una clave para encriptar y desencriptar datos
 
 
 # Obtiene la conexión a la base de datos
-host = 'localhost'
-port = '5432'
-dbname = 'usersdb-fazt'
-user = 'fast_user'
-password = 'fast_user'
+host = "localhost"
+port = "5432"
+dbname = "usersdb-fazt"
+user = "fast_user"
+password = "fast_user"
+
 
 def get_connection():
     conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
@@ -18,46 +28,62 @@ def get_connection():
 
 
 # Página inicial
-@app.route('/')
+@app.route("/")
 def hello_world():
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute('SELECT 2 + 1')
+    cursor.execute("SELECT 2 + 1")
     result = cursor.fetchone()
 
     print(result)
-    return 'Hello, World! The result is: ' + str(result)
+    return "Hello, World! The result is: " + str(result)
 
-@app.get('/api/users')
+
+@app.get("/api/users")
 def get_users():
-    return 'getting users'
+    return "getting users"
 
-@app.post('/api/users')
+
+@app.post("/api/users")
 def create_users():
     new_user = request.get_json()
-    username = new_user['username']
-    email = new_user['email']
-    password = new_user['password']
+    username = new_user["username"]
+    email = new_user["email"]
+    password = Fernet(key).encrypt(bytes(new_user["password"], "utf-8")) # le pasamos un string en utf-8, lo pasa a bits y lo encripta
 
     conn = get_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(cursor_factory=extras.RealDictCursor)
+    # con cursor_factory=extras.RealDictCursor se obtienen los datos de la base de datos como diccionarios
 
-    cursor.execute('INSERT INTO users (username, email, password) VALUES (%s, %s, %s)', (username, email, password))
+    cursor.execute(
+        "INSERT INTO users (username, email, password) VALUES (%s, %s, %s) RETURNING *",
+        (username, email, password),
+    )
+    new_creadted_user = cursor.fetchone() # fetchone() para obtener el resultado de la inserción que retorna el RETURNING
+    print(new_creadted_user)
+    conn.commit()
 
-    return 'creating users'
+    cursor.close()
+    conn.close()
 
-@app.delete('/api/users/1')
+    return jsonify(new_creadted_user)
+
+
+@app.delete("/api/users/1")
 def delete_users():
-    return 'deliting users'
+    return "deliting users"
 
-@app.put('/api/users/1')
+
+@app.put("/api/users/1")
 def update_users():
-    return 'updating users'
+    return "updating users"
 
-@app.get('/api/users/1')
+
+@app.get("/api/users/1")
 def get_user():
-    return 'getting user 1'
+    return "getting user 1"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
